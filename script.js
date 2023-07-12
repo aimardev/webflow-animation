ccb160e25f2c7d3686b60a93d97a944e7bccc02b
amiardev <aimarnino.io@gmail.com>
2023-07-12 02:12:40 +0000

    init

---
+++ script.js
@@ -0,0 +1,17 @@
+var wave1 = $('#feel-the-wave').wavify({
+  height: 80,
+  bones: 4,
+  amplitude: 60,
+  strokeColor: '#B289EF',
+  speed: .15
+});
+
+var wave2 = $('#feel-the-wave-two').wavify({
+  height: 60,
+  bones: 3,
+  amplitude: 40,
+  strokeColor: 'rgba(150, 97, 255, .8)',
+  speed: .25
+});
+
+var colors = ['rgba(255, 281, 75, .8)', '#dc75ff', '#9d9ade', '#6cd7ee', '#aceeae']
